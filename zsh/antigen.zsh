8fe57cd287313258d7096c2ddae5643f234dbd04
Justin Downing <justin@downing.us>
2020-09-23 23:34:40 -0400

    Changes (again) (#2)
    * kaf
    
    * rbenv.symlink
    
    * fix
    
    * new theme
    
    * updates!

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -3,14 +3,12 @@ source `brew --prefix`/share/antigen/antigen.zsh
 antigen bundle --loc=lib
 
 antigen bundles <<BUNDLES
-  aws
   brew
   bundler
   docker
   gem
   git
   ruby
-  terraform
   zsh-users/zsh-completions
   zsh-users/zsh-syntax-highlighting
   rupa/z
