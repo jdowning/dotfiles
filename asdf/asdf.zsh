92b8667047599cd7eb5704bea39b61fec7f42a37
Justin Downing <justin@downing.us>
2022-07-06 13:12:21 -0700

    improvements to support zsh4humans

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,2 +1,2 @@
-. $(brew --prefix asdf)/libexec/asdf.sh
+z4h source -- ${HOMEBREW_PREFIX:+$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh}
 export ASDF_RUBY_BUILD_VERSION=master
--- psql/psql.zsh
+++
@@ -1,1 +0,0 @@
-export PATH="/usr/local/opt/postgresql@13/bin:$PATH"
