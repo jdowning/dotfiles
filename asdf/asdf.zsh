f99168c705cad9811099f024e418412a4651da82
Justin Downing <justin@downing.us>
2024-11-18 15:43:22 -0800

    updates

--- asdf/asdf.zsh
+++ asdf/asdf.zsh
@@ -1,2 +1,2 @@
-z4h source -- ${HOMEBREW_PREFIX:+$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh}
-export ASDF_RUBY_BUILD_VERSION=master
+# z4h source -- ${HOMEBREW_PREFIX:+$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.sh}
+# export ASDF_RUBY_BUILD_VERSION=master
